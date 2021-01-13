e3872cca3caf917b0801b116150bf64a31f6a937
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2021-01-13 14:42:57 -0300

    RMB-0: Inicializar luego de que la ventana cargue, no antes

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,69 +1,73 @@
 import { Mapa } from "./Mapa";
 import { Nivel } from "./Nivel";
 
-const mapa = new Mapa(document.querySelector("#map"))
+window.onload = inicializar
 
-mapa.iniciarlizar()
+function inicializar() {
+    const mapa = new Mapa(document.querySelector("#map"))
 
-// Revisar si hay un distrito/seccion enfocado
-//  Si la hay, enfocar el mapa en el feature correspondiente
-//  Emito evento LW con el ID del distrito/seccion
-
-window['mapa'] = mapa
-
-const botonEnfocarDistritos: HTMLButtonElement = document.querySelector("#showDistritos")
-botonEnfocarDistritos.onclick = () => {
-    mapa.enfocarDistritos()
-}
-
-const botonEnfocarSecciones: HTMLButtonElement = document.querySelector("#showSecciones")
-botonEnfocarSecciones.onclick = () => {
-    mapa.enfocarSecciones()
-}
-
-const showMapStreets: HTMLInputElement = document.querySelector("#showMapStreets")
-showMapStreets.onchange = () => {
-    if (showMapStreets.checked) {
-        mapa.mostrarCalles()
-    } else {
-        mapa.ocultarCalles()
+    mapa.iniciarlizar()
+    
+    // Revisar si hay un distrito/seccion enfocado
+    //  Si la hay, enfocar el mapa en el feature correspondiente
+    //  Emito evento LW con el ID del distrito/seccion
+    
+    window['mapa'] = mapa
+    
+    const botonEnfocarDistritos: HTMLButtonElement = document.querySelector("#showDistritos")
+    botonEnfocarDistritos.onclick = () => {
+        mapa.enfocarDistritos()
     }
-}
-
-const initialShow: HTMLButtonElement = document.querySelector("#initialShow")
-initialShow.onclick = () => {
-    mapa.ocultarDistritosEnfocados()
-    mapa.mostrarSecciones()
-    mapa.enfocarBuenosAires()
-}
-
-const select: HTMLSelectElement = document.querySelector("#idSecciones")
-select.onchange = () => {
-    const id = Number(select.value)
-    if (id === -1) {
-        switch (mapa.nivel) {
-            case Nivel.TODAS_LAS_SECCIONES:
-                mapa.enfocarSecciones()
-                break
-            case Nivel.TODOS_LOS_DISTRITOS:
-                mapa.enfocarDistritos()
-                break
-            case Nivel.UNA_SECCION:
-                break
-            case Nivel.UN_DISTRITO:
-                break
+    
+    const botonEnfocarSecciones: HTMLButtonElement = document.querySelector("#showSecciones")
+    botonEnfocarSecciones.onclick = () => {
+        mapa.enfocarSecciones()
+    }
+    
+    const showMapStreets: HTMLInputElement = document.querySelector("#showMapStreets")
+    showMapStreets.onchange = () => {
+        if (showMapStreets.checked) {
+            mapa.mostrarCalles()
+        } else {
+            mapa.ocultarCalles()
         }
-    } else {
-        switch (mapa.nivel) {
-            case Nivel.TODAS_LAS_SECCIONES:
-                mapa.enfocarSeccionPorId(id)
-                break
-            case Nivel.TODOS_LOS_DISTRITOS:
-                mapa.enfocarDistritoPorId(id)
-                break
-            case Nivel.UNA_SECCION:
-                mapa.enfocarDistritoPorId(id)
-                break
+    }
+    
+    const initialShow: HTMLButtonElement = document.querySelector("#initialShow")
+    initialShow.onclick = () => {
+        mapa.ocultarDistritosEnfocados()
+        mapa.mostrarSecciones()
+        mapa.enfocarBuenosAires()
+    }
+    
+    const select: HTMLSelectElement = document.querySelector("#idSecciones")
+    select.onchange = () => {
+        const id = Number(select.value)
+        if (id === -1) {
+            switch (mapa.nivel) {
+                case Nivel.TODAS_LAS_SECCIONES:
+                    mapa.enfocarSecciones()
+                    break
+                case Nivel.TODOS_LOS_DISTRITOS:
+                    mapa.enfocarDistritos()
+                    break
+                case Nivel.UNA_SECCION:
+                    break
+                case Nivel.UN_DISTRITO:
+                    break
+            }
+        } else {
+            switch (mapa.nivel) {
+                case Nivel.TODAS_LAS_SECCIONES:
+                    mapa.enfocarSeccionPorId(id)
+                    break
+                case Nivel.TODOS_LOS_DISTRITOS:
+                    mapa.enfocarDistritoPorId(id)
+                    break
+                case Nivel.UNA_SECCION:
+                    mapa.enfocarDistritoPorId(id)
+                    break
+            }
         }
     }
 }
