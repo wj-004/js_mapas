a85d330ab8cd915627a244d4a586a8d73340c4be
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2021-01-17 23:19:46 -0300

    RMB-0: cartel de carga

--- src/typescript/app/mapa/crearMapa.ts
+++ src/typescript/app/mapa/crearMapa.ts
@@ -1,13 +1,8 @@
+import VectorLayer from "ol/layer/Vector";
 import { Mapa } from "../../mapa/Mapa";
 import { getLayers } from "../../util/getLayer";
 
-export async function crearMapa(): Promise<Mapa> {
-    const capas = await getLayers([
-        '../data/vector_data/bsas_provincia_distritos.geojson',
-        '../data/vector_data/bsas_provincia_secciones.geojson',
-        '../data/vector_data/contorno_relleno.geojson'
-    ]);
-
+export function crearMapa(capas: VectorLayer[]): Mapa {
     return new Mapa(
         document.querySelector("#map"),
         document.querySelector("#idSecciones"),
--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -3,12 +3,29 @@ import { configurarListenersDelMapa } from "./app/mapa/configurarListenersDelMap
 import { crearMapa } from "./app/mapa/crearMapa";
 import { enfocarZonaGuardada } from "./app/mapa/enfocarFeatureGuardado";
 import { configurarListenersDelNavegador } from "./app/navegador/configurarListenersDelNavegador";
+import { getLayers } from "./util/getLayer";
 import { livewireEmit } from "./util/livewireEmit";
 
 window.onload = inicializar
 
 async function inicializar() {
-    const mapa = await crearMapa()
+    // Esperar que se descarguen los datos para el mapa
+    const capas = await getLayers([
+        '../data/vector_data/bsas_provincia_distritos.geojson',
+        '../data/vector_data/bsas_provincia_secciones.geojson',
+        '../data/vector_data/contorno_relleno.geojson'
+    ]);
+
+    // Quitar dialogo de carga
+    const dialogoCargaContainer = document.querySelector('.dialogo-carga--container')
+    dialogoCargaContainer.classList.add('d-none')
+    dialogoCargaContainer.classList.remove('d-flex')
+    
+    // Mostrar mapa y sus controles
+    document.querySelector('.mapa--container').classList.remove('d-none')
+    document.querySelector('.mapa--controles').classList.remove('d-none')
+
+    const mapa = crearMapa(capas)
     
     configurarListenersDelMapa(mapa)
     configurarElementosDeInterfaz(mapa)
